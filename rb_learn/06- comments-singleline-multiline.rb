40a25355c485adf2c1ce42d328e02a2bab5522bf
abhilashvpillai <abhilash_vpillai@yahoo.com>
2015-10-24 22:17:19 +0530

    ruby changes

--- rb_learn/06- comments-singleline-multiline.rb
+++ rb_learn/06- comments-singleline-multiline.rb
@@ -1,6 +1,5 @@
 # this is a single line comment
 
-
 =begin
 	this is a multiline comment
 	the # sign is not needed anymore!
@@ -9,3 +8,18 @@
 my_name = "Abhilash"
 
 puts my_name
+
+=begin
+	these are ruby multiline comments......
+	looks weird in WScite
+=end
+
+puts "Prints this string on the screen!!!"
+
+test_var = my_name
+
+puts test_var
+
+=begin
+	Another Example Here
+=end
